bc0efb50b2e43d68b32e9260d7f98b9e4f6a2fe3
Eswar Saladi <hello@generalist.dev>
2024-03-18 00:40:49 -0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import './global.css';
-import type { Metadata } from 'next';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-import { Navbar } from './components/nav';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { SandpackCSS } from './blog/[slug]/sandpack';
+import "./global.css";
+import "./globals.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SandpackCSS } from "./blog/[slug]/sandpack";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "./lib/utils";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://leerob.io'),
+  metadataBase: new URL("https://generalist.dev"),
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: "Generalist Dev",
+    template: "%s | Generalist Dev",
   },
-  description: 'Developer, writer, and creator.',
+  description: "Developer, writer, and creator.",
   openGraph: {
-    title: 'Lee Robinson',
-    description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
-    locale: 'en_US',
-    type: 'website',
+    title: "Generalist Dev",
+    description: "Developer, writer, and creator.",
+    url: "https://generalist.dev",
+    siteName: "Generalist Dev",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -28,37 +31,24 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   twitter: {
-    title: 'Lee Robinson',
-    card: 'summary_large_image',
+    title: "Generalist Dev",
+    card: "summary_large_image",
   },
   verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
+    google: "eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw",
+    yandex: "14d2e73487fa6c71",
   },
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(' ');
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
+    <html lang="en" className={cn("text-black bg-white dark:text-white dark:bg-[#111010]", GeistSans.variable, GeistMono.variable)}>
       <head>
         <SandpackCSS />
       </head>
